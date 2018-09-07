265c4dafcbe40d02f0d591b1bf6463e1caab5411
jyd <scemsjyd@vip.qq.com>
2018-09-07 09:34:35 +0800

    std io

--- main.go
+++ main.go
@@ -6,5 +6,9 @@ func main() {
 	//std_io.ReadAt()
 	//std_io.WriterAt()
 	//std_io.ReaderFrom()
-	std_io.WriterTo()
+	//std_io.WriterTo()
+	//std_io.ByteWriter()
+	//std_io.LimitReader()
+	//std_io.Stringer()
+	std_io.Scanner()
 }
--- std-io/io.go
+++ std-io/io.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 	"bufio"
+	"bytes"
+	"io"
 )
 
 // 从下标为2的地方读取指定大小的字节
@@ -48,7 +50,7 @@ func ReaderFrom() {
 
 func WriterTo() {
 	file, err := os.Open("writeAt.txt")
-	otherFile, err2 := os.Create("write2.txt")
+	otherFile, err2 := os.Create("writeAt2.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -59,4 +61,68 @@ func WriterTo() {
 	defer otherFile.Close()
 	reader := bufio.NewReader(file)
 	reader.WriteTo(otherFile)
+}
+
+func ByteWriter() {
+	var ch byte
+	fmt.Scanf("%c\n", &ch)
+
+	buffer := new(bytes.Buffer)
+	err := buffer.WriteByte(ch)
+	if err == nil {
+		fmt.Println("写入一个字节成功！准备读取该字节……")
+		newCh, _ := buffer.ReadByte()
+		fmt.Printf("读取的字节：%c\n", newCh)
+	} else {
+		fmt.Println("写入错误")
+	}
+}
+
+func ByteScanner() {
+	buffer := bytes.NewBuffer([]byte{'a', 'b'})
+	buffer.ReadByte()
+	err := buffer.UnreadByte()
+
+	err = buffer.UnreadByte()
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func LimitReader() {
+	content := "This Is LimitReader Example"
+	reader := strings.NewReader(content)
+	limitReader := io.LimitedReader{R: reader, N: 8}
+	for limitReader.N > 0 {
+		tmp := make([]byte, 2)
+		limitReader.Read(tmp)
+		fmt.Printf("%s", tmp)
+	}
+}
+
+type Person struct {
+	Name string
+	Age  int
+	Sex  int
+}
+
+func Stringer() {
+	p := &Person{"polaris", 28, 0}
+	fmt.Println(p)
+}
+
+func Scanner() {
+	file, err := os.Create("scanner.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	file.WriteString("http://studygolang.com.\nIt is the home of gophers.\nIf you are studying golang, welcome you!")
+	// 将文件 offset 设置到文件开头
+	file.Seek(0, os.SEEK_SET)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
 }
